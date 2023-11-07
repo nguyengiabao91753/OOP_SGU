368b843433f5b185f8c5d9fa513e81c6b9a56daa
unknown <pntkha250804.nvtroi1922@gmail.com>
2023-11-07 17:09:35 +0700

    hien thi QLHK va hien thi QLNV

--- program.java
+++ program.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class program {
+    static Scanner inp = new Scanner(System.in);
     public static void main(String[] args) {
         int chay = 1;
         int luachon;
         int again = 1;
-        Scanner inp = new Scanner(System.in);
+        
         quanly_ve danhsachve = new quanly_ve();
         do {
-            Scanner nhap = new Scanner(System.in);
             System.out.println("Chủ đề: Quản lý bán vé máy bay ");
             System.out.println("    1. Quản lý Hành khách");
             System.out.println("    2. Quản lý nhân viên");
@@ -23,14 +23,77 @@ public class program {
             System.out.println("    11. Thoát");
             System.out.println(">---------------------------------------<");
             System.out.println("Nhập lựa chọn");
-            int chon = nhap.nextInt();
+            int chon = inp.nextInt();
             if (chon < 1 && chon > 11) {
                 System.out.println("Lựa chọn không hợp lệ\nXin mời chọn lại");
                 continue;
             }
             switch (chon) {
                 case 1:
-
+                    System.out.println("Quản lý Hành khách:");
+                    int chon1;
+                    DSHK dshk = new DSHK();
+                    do{
+                        System.out.println(">Menu-------------------------<");
+                        System.out.println("1.Khoi tao mot danh sach moi.");
+                        System.out.println("2.Xuat danh sach hanh khach.");
+                        System.out.println("3.Them hanh khach vao danh sach.");
+                        System.out.println("4.Xoa hanh khach khoi danh sach theo ma.");
+                        System.out.println("0.Tro lai trang truoc.");
+                        System.out.println("Hay nhap lua chon.");
+                        chon1 = inp.nextInt();
+                        switch(chon1){
+                            case 1:
+                                dshk.nhap();
+                                break;
+                            case 2:
+                                dshk.xuat();
+                                break;
+                            case 3:
+                                dshk.them();
+                                break;
+                            case 4:
+                                dshk.xoa();
+                                break;
+                            case 0: break;
+                            default:
+                                System.out.println("Lua cho khong hop le\n");
+                                break;
+                        }
+                    }while(chon1 != 0);
+                    break;
+                case 2:
+                    System.out.println("Quản lý nhân viên:");
+                    int chon2;
+                    DSNV dsnv = new DSNV();
+                    do{
+                        System.out.println(">Menu-------------------------<");
+                        System.out.println("1.Khoi tao mot danh sach moi.");
+                        System.out.println("2.Xuat danh sach nhan vien.");
+                        System.out.println("3.Them nhan vien vao danh sach.");
+                        System.out.println("4.Xoa nhan vien khoi danh sach theo ma.");
+                        System.out.println("0.Tro lai trang truoc.");
+                        System.out.println("Hay nhap lua chon.");
+                        chon2 = inp.nextInt();
+                        switch(chon2){
+                            case 1:
+                                dsnv.nhap();
+                                break;
+                            case 2:
+                                dsnv.xuat();
+                                break;
+                            case 3:
+                                dsnv.them();
+                                break;
+                            case 4:
+                                dsnv.xoa();
+                                break;
+                            case 0: break;
+                            default:
+                                System.out.println("Lua cho khong hop le\n");
+                                break;
+                        }
+                    }while(chon2 != 0);
                     break;
                 case 4:            
                     System.out.println("Quản lý Chuyến bay");
